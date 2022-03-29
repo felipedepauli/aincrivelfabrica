249679beb5837acfe0201d7f9298d8094b294d59
Felipe de Pauli <fcdp83@gmail.com>
2022-03-28 21:33:23 -0300

    Symbolic Link

---
+++ static/js/4.54b43923.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbako=this.webpackJsonpbako||[]).push([[4],{129:function(t,a,s){"use strict";s.r(a),a.default=s.p+"static/media/the-truth-about-design.2f0caa6b.md"}}]);
+//# sourceMappingURL=4.54b43923.chunk.js.map
---
+++ static/js/8.0c809a28.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbako=this.webpackJsonpbako||[]).push([[8],{133:function(a,t,s){"use strict";s.r(t),t.default=s.p+"static/media/creative-bulb.aaa1dcb4.md"}}]);
+//# sourceMappingURL=8.0c809a28.chunk.js.map
