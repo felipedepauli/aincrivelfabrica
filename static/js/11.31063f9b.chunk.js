d252abb49f99ad5026bc4d8fc8f43ffeff845549
Felipe de Pauli <fcdp83@gmail.com>
2022-03-28 22:08:57 -0300

    Site working

---
+++ static/js/11.31063f9b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbako=this.webpackJsonpbako||[]).push([[11],{136:function(s,a,t){"use strict";t.r(a),a.default=t.p+"static/media/work-space.7348c192.md"}}]);
+//# sourceMappingURL=11.31063f9b.chunk.js.map
